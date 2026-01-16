ef8e5fd386be69fb24c0856acc103d3deaf6a1cc
Pedro-H-Martini <pedro.martini@pacgroup.com>
2026-01-16 11:18:43 -0300

    added dark mode

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
     return (
         <header className="border-b border-border backdrop-blur supports-backdrop-filter:bg-zinc-800">
             <div className="flex h-14 items-center px-3">
-                <AppLogo />
+                <AppLogo className="w-50 h-auto" />
             </div>
         </header>
     )

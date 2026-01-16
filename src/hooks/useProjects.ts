aa65cbeefe1765ffd7fdc581ac68c9c451504d66
Pedro-H-Martini <pedro.martini@pacgroup.com>
2026-01-16 11:23:13 -0300

    renamed getProject hook to getProjectByUuid

--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import { getProject } from "@/api/projects";
+import { getProjectByUuid } from "@/api/projects";
 import type { Project } from "@/types";
 
 export function useProject(projectUuid: string | undefined) {
     return useQuery<Project>({
         queryKey: ["projects", projectUuid],
-        queryFn: async () => await getProject(projectUuid!),
+        queryFn: async () => await getProjectByUuid(projectUuid!),
         enabled: !!projectUuid,
     });
 }
